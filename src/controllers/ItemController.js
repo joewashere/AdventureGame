165a927fafae0ec617898f904c4db731ca45b56b
joewashere <7690921+joewashere@users.noreply.github.com>
2023-02-01 19:12:34 -0800

    Added coin item to test item controller

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -1,11 +1,14 @@
+import CoinItem from "../models/CoinItem.js";
 import HeartItem from "../models/HeartItem.js";
 
 export default class ItemController {
     constructor () {
         this.heart = new HeartItem({x:100, y:100, w:10, h:10});
+        this.coin = new CoinItem({x:115, y:100, w:10, h:10});
     }
 
     update (c) {
         this.heart.update(c);
+        this.coin.update(c);
     }
 }
---
+++ src/models/CoinItem.js
@@ -0,0 +1,13 @@
+import { importSprite } from "../helpers/importSprite.js";
+import Item from "./Item.js";
+
+export default class CoinItem extends Item {
+    constructor ({x, y, w, h}) {
+        super({x, y, w, h});
+        this.sprite = importSprite('../../assets/Other/Coin.png');
+    }
+
+    update (c) {
+        super.update(c);
+    }
+}
